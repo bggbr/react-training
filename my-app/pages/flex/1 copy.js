b08e1985b80051777019aa28eb9a8a7797a30006
bggbr <jaejung603@gmail.com>
2022-09-02 22:33:20 +0900

    alternating grid

--- my-app/pages/flex/1 copy.js
+++ my-app/pages/flex/1 copy.js
@@ -1,7 +1,7 @@
 export default function Flex_First() {
     return (
-        <div className="space-y-4 text-white text-center">
-            <h1 className="text-black">Stretch all, fixed spacing</h1>
+        <div className="space-y-4">
+            <h1>Stretch all, fixed spacing</h1>
             <div className="flex space-x-2">
                 <div className="h-full grow bg-blue-700">1</div>
                 <div className="h-full grow bg-blue-700">2</div>
@@ -23,13 +23,12 @@ export default function Flex_First() {
                 <div className="h-full grow bg-blue-700">4</div>
                 <div className="h-full grow bg-blue-700">5</div>
             </div>
-            <h1 className="text-black">Alternating grid</h1>
-            <div className="flex flex-wrap justify-between">
-                <div className="w-5/12 h-100px mb-2 bg-blue-300">1</div>
-                <div className="w-5/12 h-100px mb-2 bg-blue-300">2</div>
-                <div className="w-full  h-100px mb-2 bg-blue-300">3</div>
-                <div className="w-5/12 h-100px mb-2 bg-blue-300">4</div>
-                <div className="w-5/12 h-100px mb-2 bg-blue-300">5</div>
+            <div className="flex space-x-2">
+                <div className="h-full grow bg-blue-700">1</div>
+                <div className="h-full grow bg-blue-700">2</div>
+                <div className="h-full grow-[3] bg-blue-700">3</div>
+                <div className="h-full grow bg-blue-700">4</div>
+                <div className="h-full grow bg-blue-700">5</div>
             </div>
         </div>
     );
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Button from '../src/component/Button';
 import styles from '../styles/Home.module.css';
+import Flex_First from './flex/1';
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <Button>asdf</Button>
             <Button disabled={false}>asdf</Button>
+            <Flex_First></Flex_First>
         </div>
     );
 }
