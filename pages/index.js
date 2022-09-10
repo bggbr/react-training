f5423fb1764643f9dd68ab90ea5d8ae04eb7c44e
bggbr <jaejung603@gmail.com>
2022-09-10 19:55:59 +0900

    props를 이용한 카테고리 상태값 및 메뉴 보여주기

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,71 @@
+import React, { useState, useEffect } from 'react';
 import Category from '../src/component/Category';
 import Header from '../src/component/Header';
 import Menu from '../src/component/Menu';
 
 function Home() {
-    return (
-        <div>
-            <Header />
-            <div className="flex">
-                <Category />
-                <Menu />
-            </div>
-        </div>
-    );
+	const [isCategory, setIsCategory] = useState('');
+	const [isMenuList, setIsMenuList] = useState('');
+	const [allMenuList, setAllMenuList] = useState([
+		{
+			id: 1,
+			name: '김치찌개',
+			cookingTime: 3,
+			price: 7000,
+			category: 'korean',
+		},
+		{
+			id: 2,
+			name: '된장찌개',
+			cookingTime: 10,
+			price: 8000,
+			category: 'korean',
+		},
+		{
+			id: 3,
+			name: '초밥',
+			cookingTime: 5,
+			price: 9000,
+			category: 'japaness',
+		},
+	]);
+
+	const setCategory = (category) => {
+		setIsCategory(category);
+	};
+
+	const setMenu = (menu) => {
+		setAllMenuList([...allMenuList, menu]);
+	};
+
+	const menuCount = (nation) => {
+		return allMenuList.filter((el) => el.category === nation).length;
+	};
+
+	useEffect(() => {
+		let menu = allMenuList.filter((el) => el.category === isCategory);
+		setIsMenuList(menu);
+	}, [allMenuList, isCategory]);
+
+	return (
+		<div>
+			<div>{isCategory}</div>
+			<div>{allMenuList.length}</div>
+			{/* <div>
+				{allMenuList &
+					allMenuList.map((item, idx) => {
+						<div key={idx}>
+							<div>{item}</div>
+						</div>;
+					})}
+			</div> */}
+			<Header />
+			<div className='flex'>
+				<Category setCategory={setCategory} menuCount={menuCount} />
+				<Menu menuList={isMenuList} setMenu={setMenu} isCategory={isCategory} />
+			</div>
+		</div>
+	);
 }
 
 export default Home;
--- src/component/Category.js
+++ src/component/Category.js
@@ -1,12 +1,31 @@
+import react, { useState, useEffect } from 'react';
 import Button from './Button';
 
-export default function Category() {
-    return (
-        <div className="bg-slate-800 text-white w-52 p-5">
-            <h2 className="font-bold mb-5">카테고리</h2>
-            <Button>한식</Button>
-            <Button>일식</Button>
-            <Button>중식</Button>
-        </div>
-    );
+export default function Category({ setCategory, menuCount }) {
+	const [isState, setIsState] = useState('korean');
+
+	const selectCategory = (e) => {
+		setIsState(e.target.value);
+	};
+
+	let koreanMenuCount = useEffect(() => {
+		setCategory(isState);
+	}, [isState]);
+
+	return (
+		<div className='bg-slate-800 text-white w-52 p-5'>
+			<h2 className='font-bold mb-5'>카테고리</h2>
+			<div className='flex flex-col space-y-5'>
+				<Button className={isState === 'korean' ? 'bg-neutral-400' : ''} onClick={selectCategory} value='korean'>
+					한식 ({menuCount('korean')})
+				</Button>
+				<Button className={isState === 'japaness' ? 'bg-neutral-400' : ''} onClick={selectCategory} value='japaness'>
+					일식 ({menuCount('japaness')})
+				</Button>
+				<Button className={isState === 'chinese' ? 'bg-neutral-400' : ''} onClick={selectCategory} value='chinese'>
+					중식 ({menuCount('chinese')})
+				</Button>
+			</div>
+		</div>
+	);
 }
