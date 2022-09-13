bb4a3c50a5c47c5a386fce90c443bbb197e75afd
bggbr <jaejung603@gmail.com>
2022-09-13 23:46:45 +0900

    메뉴 삭제 기능

--- pages/index.js
+++ pages/index.js
@@ -5,53 +5,58 @@ import Menu from '../src/component/Menu';
 import { MenuContext, SetMenuContext } from '../src/context/MenuContext';
 
 function Home() {
-	const [category, setCategory] = useState('korean');
-	const [allMenuList, setAllMenuList] = useState([
-		{
-			id: 1,
-			name: '김치찌개',
-			cookingTime: 3,
-			price: 7000,
-			category: 'korean',
-		},
-		{
-			id: 2,
-			name: '된장찌개',
-			cookingTime: 10,
-			price: 8000,
-			category: 'korean',
-		},
-		{
-			id: 3,
-			name: '초밥',
-			cookingTime: 5,
-			price: 9000,
-			category: 'japaness',
-		},
-	]);
-	const [cookingMenu, setCookingMenu] = useState([]);
+    const [category, setCategory] = useState('korean');
+    const [allMenuList, setAllMenuList] = useState([
+        {
+            id: 1,
+            name: '김치찌개',
+            cookingTime: 3,
+            price: 7000,
+            category: 'korean',
+        },
+        {
+            id: 2,
+            name: '된장찌개',
+            cookingTime: 10,
+            price: 8000,
+            category: 'korean',
+        },
+        {
+            id: 3,
+            name: '초밥',
+            cookingTime: 5,
+            price: 9000,
+            category: 'japaness',
+        },
+    ]);
+    const [cookingMenu, setCookingMenu] = useState([]);
 
-	const setMenu = (menu) => {
-		setAllMenuList([...allMenuList, menu]);
-	};
+    const addMenu = (menu) => {
+        setAllMenuList([...allMenuList, menu]);
+    };
 
-	const setCookingList = (cookMenu) => {
-		setCookingMenu([...cookingMenu, cookMenu]);
-	};
+    const removeMenu = (menu) => {
+        let copyAllMenu = allMenuList.filter((el) => (el.id === menu.id ? false : true));
+        setAllMenuList([...copyAllMenu]);
+    };
 
-	return (
-		<div>
-			<MenuContext.Provider value={allMenuList}>
-				<SetMenuContext.Provider value={setAllMenuList}>
-					<Header cookingMenu={cookingMenu} />
-					<div className='flex'>
-						<Category category={category} setCategory={setCategory} />
-						<Menu setMenu={setMenu} category={category} setCookingList={setCookingList} />
-					</div>
-				</SetMenuContext.Provider>
-			</MenuContext.Provider>
-		</div>
-	);
+    const setCookingList = (cookMenu) => {
+        setCookingMenu([...cookingMenu, cookMenu]);
+    };
+
+    return (
+        <div>
+            <MenuContext.Provider value={allMenuList}>
+                <SetMenuContext.Provider value={setAllMenuList}>
+                    <Header cookingMenu={cookingMenu} setCookingMenu={setCookingMenu} />
+                    <div className="flex">
+                        <Category category={category} setCategory={setCategory} />
+                        <Menu addMenu={addMenu} removeMenu={removeMenu} category={category} setCookingList={setCookingList} />
+                    </div>
+                </SetMenuContext.Provider>
+            </MenuContext.Provider>
+        </div>
+    );
 }
 
 export default Home;
