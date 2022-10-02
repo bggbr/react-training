2d06be852339e633cd3d23202eef18af76f97232
bggbr <jaejung603@gmail.com>
2022-10-02 17:25:11 +0900

    menu 수정 페이지

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -52,31 +52,51 @@ import { useRouter } from 'next/router';
 
 import React, { useRef, useContext } from 'react';
 import { CookingContext } from '../../src/state';
+// import Button from '../../src/component/Button.js';
+import Modal from '../../src/component/Modal.js';
 
 // 3번
 export default function MenuDetailPage({ id }) {
-    const { state, dispatch } = useContext(CookingContext);
-    console.log('test', id);
-    console.log(state);
-    return (
-        <>
-            <h1 className="font-bold text-2xl mb-4">메뉴 상세 페이지</h1>
-            <div>{id}</div>
-        </>
-    );
+	const { state, dispatch } = useContext(CookingContext);
+	const [menu, setMenu] = useState(state.menu.find((menu) => menu.id === id));
+	// console.log(menu);
+
+	return (
+		<>
+			<div className='m-10 border-l-green-500'>
+				<h1 className='mb-4'>기본 정보</h1>
+				<ul>
+					<li>ID: {menu.id}</li>
+					<li>
+						조리 시간: {menu.cookingTime}초<Modal action={'time'}>수정</Modal>
+					</li>
+					<li>
+						가격: {menu.price}원<Modal action={'price'}>수정</Modal>
+					</li>
+					<li>
+						카테고리: {menu.category}
+						<Modal action={'category'}>수정</Modal>
+					</li>
+				</ul>
+			</div>
+			<div className='m-10 border-l-indigo-600'>
+				<h2>변경 내역</h2>
+			</div>
+		</>
+	);
 }
 
 export async function getStaticProps({ params }) {
-    console.log('getStaticProps called');
-    return {
-        props: { id: Number(params.id) },
-        // revalidate: 3, // 주석 풀고 npm run build && npm start => 3초 후에 getStaticProps 호출되는 것 확인해보기
-    };
+	console.log('getStaticProps called');
+	return {
+		props: { id: Number(params.id) },
+		// revalidate: 3, // 주석 풀고 npm run build && npm start => 3초 후에 getStaticProps 호출되는 것 확인해보기
+	};
 }
 
 export async function getStaticPaths() {
-    return {
-        paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
-        fallback: true,
-    };
+	return {
+		paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+		fallback: true,
+	};
 }
--- src/state.js
+++ src/state.js
@@ -3,64 +3,73 @@ import produce from 'immer';
 
 export const CookingContext = createContext(null);
 export const initialState = {
-    CATEGORY_LIST: [
-        { id: 0, en: 'korean', kr: '한식' },
-        { id: 1, en: 'japaness', kr: '일식' },
-        { id: 2, en: 'chinese', kr: '중식' },
-    ],
-    category: 'korean',
-    menu: [
-        {
-            id: 1,
-            name: '김치찌개',
-            cookingTime: 2,
-            price: 7000,
-            category: 'korean',
-        },
-    ],
-    cookingMenu: [],
-    maxCookingNum: 3,
-    totalSales: 0,
+	CATEGORY_LIST: [
+		{ id: 0, en: 'korean', kr: '한식' },
+		{ id: 1, en: 'japaness', kr: '일식' },
+		{ id: 2, en: 'chinese', kr: '중식' },
+	],
+	category: 'korean',
+	menu: [
+		{
+			id: 1,
+			name: '김치찌개',
+			cookingTime: 2,
+			price: 7000,
+			category: 'korean',
+		},
+	],
+	cookingMenu: [],
+	maxCookingNum: 3,
+	totalSales: 0,
 };
 
 export const reducer = (_state, action) => {
-    return produce(_state, (state) => {
-        switch (action.type) {
-            case 'set-category':
-                let { nowCategory } = action;
-                state.category = nowCategory;
-                break;
-            case 'add-menu':
-                let { addedMenu } = action;
-                state.menu.push(addedMenu);
-                break;
-            case 'delete-menu':
-                let { deletedMenu } = action;
-                state.menu = state.menu.filter((el) => el.id !== deletedMenu.id);
-                break;
-            case 'add-cooking':
-                let nowCookNum = state.cookingMenu.length;
-                if (nowCookNum < state.maxCookingNum) {
-                    state.cookingMenu.push(action.cookingMenu);
-                }
-                break;
-            case 'delete-cooking':
-                let { deleteId } = action;
-                state.cookingMenu = state.cookingMenu.filter((el) => el.id !== deleteId);
-                break;
-            case 'pay-cooking':
-                let { payId, price } = action;
-                state.cookingMenu = state.cookingMenu.filter((el) => el.id !== payId);
-                state.totalSales += price;
-                break;
-            case 'plus-maxCooking':
-                state.maxCookingNum = state.maxCookingNum + 1 > 3 ? 3 : state.maxCookingNum + 1;
-                break;
-            case 'minus-maxCooking':
-                state.maxCookingNum = state.maxCookingNum - 1 < 0 ? 0 : state.maxCookingNum - 1;
-                break;
-            default:
-                return state;
-        }
-    });
+	return produce(_state, (state) => {
+		switch (action.type) {
+			case 'set-category':
+				let { nowCategory } = action;
+				state.category = nowCategory;
+				break;
+			case 'add-menu':
+				let { addedMenu } = action;
+				state.menu.push(addedMenu);
+				break;
+			case 'delete-menu':
+				let { deletedMenu } = action;
+				state.menu = state.menu.filter((el) => el.id !== deletedMenu.id);
+				break;
+			case 'add-cooking':
+				let nowCookNum = state.cookingMenu.length;
+				if (nowCookNum < state.maxCookingNum) {
+					state.cookingMenu.push(action.cookingMenu);
+				}
+				break;
+			case 'delete-cooking':
+				let { deleteId } = action;
+				state.cookingMenu = state.cookingMenu.filter((el) => el.id !== deleteId);
+				break;
+			case 'set-cookingTime':
+				console.log(action);
+				break;
+			case 'set-price':
+				console.log(action);
+				break;
+			case 'set-category':
+				console.log(action);
+				break;
+			case 'pay-cooking':
+				let { payId, price } = action;
+				state.cookingMenu = state.cookingMenu.filter((el) => el.id !== payId);
+				state.totalSales += price;
+				break;
+			case 'plus-maxCooking':
+				state.maxCookingNum = state.maxCookingNum + 1 > 3 ? 3 : state.maxCookingNum + 1;
+				break;
+			case 'minus-maxCooking':
+				state.maxCookingNum = state.maxCookingNum - 1 < 0 ? 0 : state.maxCookingNum - 1;
+				break;
+			default:
+				return state;
+		}
+	});
 };
